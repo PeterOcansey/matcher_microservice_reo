512bae712e6e7322eaf05b32478ca9df4ab202ca
peter <ocanseypeter@gmail.com>
2022-05-21 13:35:17 +0000

    Feat: Implement seeding Property table

---
+++ app/REO/Utils/Constants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\REO\Utils;
+
+class Constants
+{
+	const DEFAULT_PROPERTY_FIELDS = ['area', 'yearOfConstruction', 'rooms', 'heatingType', 'parking', 'returnActual', 'rent'];
+    const DEFAULT_HEATING_TYPES = ['gas', 'electric'];
+    const DEFAULT_PARKINGS = [true, false];
+}
--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -3,9 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Property;
+use App\REO\Utils\Constants;
 
 class PropertyFactory extends Factory
 {
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Property::class;
+
     /**
      * Define the model's default state.
      *
@@ -14,7 +24,18 @@ class PropertyFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'address' => $this->faker->address(),
+            'property_type' => "sfsdflasdjfsdf-dfsfsdfsdf-sdfsfsf-sdfsdf",
+            'fields' =>  json_encode([
+                Constants::DEFAULT_PROPERTY_FIELDS[0] => $this->faker->numberBetween(100,999),
+                Constants::DEFAULT_PROPERTY_FIELDS[1] => $this->faker->numberBetween(1999,2022),
+                Constants::DEFAULT_PROPERTY_FIELDS[2] => $this->faker->numberBetween(1,20),
+                Constants::DEFAULT_PROPERTY_FIELDS[3] => $this->faker->randomElement(Constants::DEFAULT_HEATING_TYPES),
+                Constants::DEFAULT_PROPERTY_FIELDS[4] => $this->faker->randomElement(Constants::DEFAULT_PARKINGS),
+                Constants::DEFAULT_PROPERTY_FIELDS[5] => $this->faker->randomFloat(1,10.5, 99.9),
+                Constants::DEFAULT_PROPERTY_FIELDS[6] => $this->faker->numberBetween(1000,9999),   
+            ])
         ];
     }
 }
